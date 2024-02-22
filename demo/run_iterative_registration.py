ffbc55c13e8086017f923f67a6ca8cbba8858f54
Aakaash Radhoe <71009407+aakaashradhoe@users.noreply.github.com>
2024-02-22 13:30:16 +0100

    fixed some visualization bugs

--- demo/run_iterative_registration.py
+++ demo/run_iterative_registration.py
@@ -11,6 +11,29 @@ from plant_registration import non_rigid_registration as nrr
 from plant_registration.iterative_registration import iterative_registration
 from plant_registration import visualize as vis
 import os
+
+def plot_skeleton(ax, S, color='blue'):
+    # ax = fh.add_subplot(projection='3d')
+    ax.scatter(S.XYZ[:,0], S.XYZ[:,1], S.XYZ[:,2], 'o', color=color, depthshade=False)
+
+    N = S.A.shape[0]
+    for i in range(N):
+        for j in range(N):
+            if S.A[i,j] == 1:
+                ax.plot([S.XYZ[i,0], S.XYZ[j,0]], [S.XYZ[i,1], S.XYZ[j,1]], [S.XYZ[i,2], S.XYZ[j,2]], color)
+def plot_skeleton_correspondences(ax, S1, S2, corres, color='red'):
+    # ax = fh.add_subplot(projection='3d')
+    ind_remove = np.where(corres[:,0]==-1)
+    corres = np.delete(corres, ind_remove, axis=0)
+    ind_remove = np.where(corres[:,1]==-1)
+    corres = np.delete(corres, ind_remove, axis=0)
+
+    N = corres.shape[0]
+    for i in range(N):
+        ax.plot([S1.XYZ[corres[i,0],0], S2.XYZ[corres[i,1],0]],
+                [S1.XYZ[corres[i,0],1], S2.XYZ[corres[i,1],1]],
+                [S1.XYZ[corres[i,0],2], S2.XYZ[corres[i,1],2]], color)
+
 # source directory
 path_file = os.path.dirname(os.path.realpath(__file__))
 # %% load skeleton data
@@ -23,8 +46,10 @@ S2 = skel.Skeleton.read_graph(skel_path.format(species, day2))
 
 # visualize input data
 fh1 = plt.figure()
-vis.plot_skeleton(fh1, S1, 'b')
-vis.plot_skeleton(fh1, S2, 'r')
+ax1 = fh1.add_subplot(111, projection="3d")
+plot_skeleton(ax1, S1, 'b')
+plot_skeleton(ax1, S2, 'r')
+plt.title("Initial skeleton")
 plt.show()
 
 # %% compute non-rigid registration params
@@ -64,9 +89,10 @@ S2_hat = nrr.apply_registration_params_to_skeleton(S1, T12)
 
 # %% visualize registration results
 fh = plt.figure()
-vis.plot_skeleton(fh, S1,'b');
-vis.plot_skeleton(fh, S2_hat,'k');
-vis.plot_skeleton(fh, S2,'r');
-vis.plot_skeleton_correspondences(fh, S2_hat, S2, corres)
+ax = fh.add_subplot(111, projection="3d")
+plot_skeleton(ax, S1,'b')
+plot_skeleton(ax, S2_hat,'k')
+plot_skeleton(ax, S2,'r')
+plot_skeleton_correspondences(ax, S2_hat, S2, corres)
 plt.title("Skeleton registration results")
 
--- demo/run_skeleton_matching.py
+++ demo/run_skeleton_matching.py
@@ -4,13 +4,35 @@
 This script provides a demo for the skeleton matching (correspondence estimation) between a pair of skeletons.
 """
 
-import os 
-
+import os
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from plant_registration import skeleton as skel
 from plant_registration import skeleton_matching as skm
 from plant_registration import visualize as vis
+import numpy as np
 
+def plot_skeleton(ax, S, color='blue'):
+    # ax = fh.add_subplot(projection='3d')
+    ax.scatter(S.XYZ[:,0], S.XYZ[:,1], S.XYZ[:,2], 'o', color=color, depthshade=False)
+
+    N = S.A.shape[0]
+    for i in range(N):
+        for j in range(N):
+            if S.A[i,j] == 1:
+                ax.plot([S.XYZ[i,0], S.XYZ[j,0]], [S.XYZ[i,1], S.XYZ[j,1]], [S.XYZ[i,2], S.XYZ[j,2]], color)
+def plot_skeleton_correspondences(ax, S1, S2, corres, color='red'):
+    # ax = fh.add_subplot(projection='3d')
+    ind_remove = np.where(corres[:,0]==-1)
+    corres = np.delete(corres, ind_remove, axis=0)
+    ind_remove = np.where(corres[:,1]==-1)
+    corres = np.delete(corres, ind_remove, axis=0)
+
+    N = corres.shape[0]
+    for i in range(N):
+        ax.plot([S1.XYZ[corres[i,0],0], S2.XYZ[corres[i,1],0]],
+                [S1.XYZ[corres[i,0],1], S2.XYZ[corres[i,1],1]],
+                [S1.XYZ[corres[i,0],2], S2.XYZ[corres[i,1],2]], color)
 
 # current path of the script
 path_file = os.path.dirname(os.path.realpath(__file__))
@@ -26,12 +48,13 @@ S2_maize = skel.Skeleton.read_graph(skel_path.format(species, day2))
 params = {'weight_e': 0.01, 'match_ends_to_ends': True,  'use_labels' : True, 'label_penalty' : 1, 'debug': False}
 corres = skm.skeleton_matching(S1_maize, S2_maize, params)
 print("Estimated correspondences: \n", corres)
- 
+
 # visualize results
 fh = plt.figure()
-vis.plot_skeleton(fh, S1_maize,'b')
-vis.plot_skeleton(fh, S2_maize,'r')
-vis.plot_skeleton_correspondences(fh, S1_maize, S2_maize, corres) 
+# ax = fh.subplots()
+ax = fh.add_subplot(111, projection='3d')
+plot_skeleton(ax, S1_maize, 'b')
+plot_skeleton(ax, S2_maize, 'r')
+plot_skeleton_correspondences(ax, S1_maize, S2_maize, corres)
 plt.title("Estimated correspondences between skeletons")
-
 plt.show()
